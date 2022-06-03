d6bf77b629c149cfbb6479b0aa169547c8c5ae70
Jiaqi <lijq1103@163.com>
2022-06-04 03:12:16 +0800

    feat: upload

--- src/fileUpload/inputUpload.tsx
+++ src/fileUpload/inputUpload.tsx
@@ -2,6 +2,7 @@ import {useRef, memo, useState} from 'react'
 import type { UploadBaseParams } from "./types";
 import styles from './styles.module.scss'
 import Progress from "../Progress";
+import axios from 'axios'
 
 interface UploadProps extends UploadBaseParams{
   fileType?: 'img' | 'jpg' | 'png' | 'jpeg' | 'webp'
@@ -13,24 +14,43 @@ const InputUpload = (props: UploadProps) => {
   const inputFileRef = useRef<HTMLInputElement | null>(null)
   const [url, setUrl] = useState<string | null>(null)
 
-  function readImage(files: FileList) {
+  // 读取图片生成 formData
+  async function readImage(files: FileList) {
     const file = files[0]
+
+    const fileName = file.name
 
     if(file.type.indexOf("image") == 0){
       // 读取图片实例
-      const reader = new FileReader();
-      // 将图片转换为base64码
-      reader.readAsDataURL(file);
-      reader.onload = function(e){
-        setUrl(e.target.result as string)
-      }
+      return new Promise((resolve => {
+        const reader = new FileReader();
+        // 将图片转换为base64码
+        reader.readAsDataURL(file);
+        reader.onload = function(e){
+          setUrl(e.target.result as string)
+
+          const formData = createFormData(fileName, file)
+          resolve(formData)
+        }
+      }))
     }
   }
 
-  function onChange (event: InputEvent) {
+  // 不借助 form 表单，生成 form data
+  function createFormData(name: string, file: File) {
+    const formData = new FormData()
+    formData.append('file', file)
+    return formData
+  }
+
+  async function onChange (event: InputEvent) {
     const fileList = inputFileRef.current?.files
     if(fileList instanceof FileList) {
-      readImage(fileList)
+      const res = await readImage(fileList)
+
+      const uploadRes = await axios.post('http://localhost:4000/upload', res)
+      console.log(uploadRes)
+      return res
     }
   }
 
