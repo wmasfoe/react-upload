89cfd88cf58b0646f0398560a698f1e63855efd8
Jiaqi <lijq1103@163.com>
2022-06-08 14:59:20 +0800

    feat: useAppend hook

--- src/hooks/useAppendState.ts
+++ src/hooks/useAppendState.ts
@@ -1,14 +1,52 @@
-import {useRef} from "react";
+import { useState } from "react";
+import { ulid } from 'ulid'
 
-function useAppendState(initState) {
-  const state = useRef(initState)
-  // 带 id 的state
-  const stateIDs = useRef()
-  function removeState() {
+function useLastingState<T>(initState) {
+  const [state, setState] = useState(initState)
 
+  const stateMap = new Map()
+
+  // 同步 map 和 state 中的数据
+  function syncData() {
+    const mapKeys = stateMap.keys()
+    const mapVal = []
+    let state = false
+    while (!state) {
+      const currentVal = mapKeys.next()
+      state = currentVal.done
+      if(!state) {
+        const val = stateMap.get(currentVal.value)
+        mapVal.push(val)
+      }
+    }
+    setState(mapVal)
+  }
+
+  function addState(newState: T) {
+    stateMap.set(ulid, newState)
+    syncData()
+  }
+
+  function removeState(key: string) {
+    if(stateMap.has(key)) {
+      // 删除 state 中的数据
+      stateMap.delete(key)
+    } else {
+      throw new Error('当前值不存在')
+    }
+    syncData()
   }
   function clearAll() {
-
+    stateMap.clear()
+    syncData()
   }
 
+  return {
+    addState,
+    state,
+    removeState,
+    clearAll
+  }
 }
+
+export default useLastingState
